d22865a0a9074dcc3f9bc0d2633dc2b1d159d856
taehuun <39759138+taehuun@users.noreply.github.com>
2023-08-26 14:10:46 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,37 +1,38 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
 
-	static int n;
-	static int[] sin = new int[11];
-	static int[] sson = new int[11];
-	static int best = Integer.MAX_VALUE;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		n = Integer.parseInt(br.readLine());
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st = null;
+	static int n, sin[], sson[], best;
 
+	static void init() throws IOException {
+		n = Integer.parseInt(br.readLine());
+		sin = new int[n];
+		sson = new int[n];
+		best = Integer.MAX_VALUE;
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			sin[i] = Integer.parseInt(st.nextToken());
 			sson[i] = Integer.parseInt(st.nextToken());
-			
 		}
-		powerSet(0, 0, 1, 0);
-		System.out.println(best);
 	}
-	private static void powerSet(int idx, int size, int sin_sum, int sson_sum) {
-		// TODO Auto-generated method stub
-		if(idx == n) {
-			if(size != 0) {
+
+	static void go(int idx, int size, int sin_sum, int sson_sum) {
+		if (idx == n) {
+			if (size != 0) {
 				best = Math.min(best, Math.abs(sin_sum - sson_sum));
 			}
 			return;
 		}
-		powerSet(idx+1, size, sin_sum, sson_sum);
-		powerSet(idx+1, size+1, sin_sum*sin[idx], sson_sum+sson[idx]);
+		go(idx + 1, size, sin_sum, sson_sum);
+		go(idx + 1, size + 1, sin_sum * sin[idx], sson_sum + sson[idx]);
 	}
-	
-	
 
+	public static void main(String[] args) throws IOException {
+		init();
+		go(0, 0, 1, 0);
+		System.out.println(best);
+	}
 }
