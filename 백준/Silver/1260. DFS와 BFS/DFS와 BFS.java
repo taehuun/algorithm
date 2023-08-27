9b259d42d2ff42e6cf22512b807ba6a6cb5466e5
taehuun <39759138+taehuun@users.noreply.github.com>
2023-08-27 17:55:33 +0900

    [Silver II] Title: DFS와 BFS, Time: 244 ms, Memory: 19108 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -4,56 +4,53 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st = null;
 	static StringBuilder sb = new StringBuilder();
-	static ArrayList<Integer>[] arr;
-	static boolean[] visit;
-	static int n, m, v, a, b;
-
-	private static void init() throws IOException{
+	static int N, M, V;
+	// 정점 갯수, 간선 갯수, 시작점
+	static ArrayList<Integer>[] list;
+	static boolean visit[];
+	
+	static void init() throws IOException{
 		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		v = Integer.parseInt(st.nextToken());
-
-		arr = new ArrayList[n + 1];
-		for (int i = 1; i <= n; i++) {
-			arr[i] = new ArrayList<>();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken());
+		list = new ArrayList[N+1];
+		for (int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
 		}
-
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			arr[a].add(b);
-			arr[b].add(a);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
 		}
-
-		for (int i = 1; i <= n; i++) {
-			Collections.sort(arr[i]);
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(list[i]);
 		}
 	}
 	
-	private static void dfs(int v) {
+	static void dfs(int v) {
+		sb.append(v).append(" ");
 		visit[v] = true;
-		System.out.print(v + " ");
-		
-		for (int i : arr[v]) {
+		for (int i : list[v]) {
 			if(!visit[i]) {
 				dfs(i);
 			}
 		}
 	}
 	
-	private static void bfs() {
+	static void bfs() {
 		ArrayDeque<Integer> que = new ArrayDeque<>();
-		que.offer(v);
-		visit[v] = true;
+		visit[V] = true;
+		que.offer(V);
 		while(!que.isEmpty()) {
-			int curr = que.poll();
-			System.out.print(curr + " ");
-			for (int i : arr[curr]) {
+			int a = que.poll();
+			sb.append(a).append(" ");
+			for (int i : list[a]) {
 				if(!visit[i]) {
-					que.offer(i);
 					visit[i] = true;
+					que.offer(i);
 				}
 			}
 		}
@@ -61,12 +58,12 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		init();
-		visit = new boolean[n+1];
-		dfs(v);
-		System.out.println();
-		visit = new boolean[n+1];
+		visit = new boolean[N+1];
+		dfs(V);
+		sb.append("\n");
+		visit = new boolean[N+1];
 		bfs();
-		System.out.println();
+		System.out.println(sb);
 	}
 
 }
