dd79bca9519365d2ac07eaf4f1965885fce23a1e
taehuun <39759138+taehuun@users.noreply.github.com>
2023-10-01 16:32:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 115 ms, Memory: 21,212 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st = null;
@@ -8,11 +9,11 @@ public class Solution {
 	
 	static void init() throws IOException{
 		st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());	//벌통들의 크기 N,
-		M = Integer.parseInt(st.nextToken());	//선택할 수 있는 벌통의 개수 M,
-		C = Integer.parseInt(st.nextToken());	//꿀을 채취할 수 있는 최대 양 C
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 		map = new int[N][N];
-		memo = new int[N][N-M+1];
+		memo = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < N; j++) {
@@ -22,47 +23,47 @@ public class Solution {
 	}
 	
 	static void solve() {
+		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N-M+1; j++) {
-				go(i, j, 0, 0, 0);		//	go(x좌표, y좌표, idx, sum, total) 조합
+				go(i, j, 0, 0, 0);
 			}
 		}
-		
-		
 		int max1 = 0;
-		int max1_x=0;
-		int max1_y=0;
-		for (int x = 0; x < N; x++) {
-			for (int y = 0; y < N-M+1; y++) {
-				if (max1 < memo[x][y]) {
-					max1 = memo[x][y];
-					max1_x = x;
-					max1_y = y;
+		int max1_x = 0, max1_y = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N-M+1; j++) {
+				if(max1 < memo[i][j]) {
+					max1 = memo[i][j];
+					max1_x = i;
+					max1_y = j;
 				}
 			}
 		}
 		
 		int max2 = 0;
-		for (int x = 0; x < N; x++) {
-			for (int y = 0; y < N-M+1; y++) {
-				if(x == max1_x && Math.abs(y - max1_y) < M) continue; // 영역이 겹치면 제외
-				if (max2 < memo[x][y]) max2 = memo[x][y];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N-M+1; j++) {
+				if(max1_x == i && (max1_y < j+M || max1_y+M < j)) continue;
+				if(max2 < memo[i][j]) {
+					max2 = memo[i][j];
+				}
 			}
 		}
 		
 		sb.append(max1+max2);
 	}
 	
-
-	
 	static void go(int x, int y, int idx, int sum, int total) {
-		if(sum > C) return; 	//C보다 크면 그 다음 조합은 볼 필요도 없음
-		if(idx == M) {		//종료조건
-			memo[x][y] = memo[x][y] > total ? memo[x][y] : total;
+		if(sum > C) return;
+		if(idx == M) {
+			if(memo[x][y] < total) {
+				memo[x][y] = total;
+			}
 			return;
 		}
-		int tmp = map[x][y+idx];
-		go(x, y, idx+1, sum+tmp, total+tmp*tmp);
+		int val = map[x][y+idx];
+		go(x, y, idx+1, sum+val, total+val*val);
 		go(x, y, idx+1, sum, total);
 	}
 
@@ -72,7 +73,6 @@ public class Solution {
 			sb.append("#").append(t).append(" ");
 			init();
 			solve();
-			
 			sb.append("\n");
 		}
 		System.out.println(sb);
