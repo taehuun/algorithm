a506a852633687c2393b0273d0f46695a345b328
taehuun <39759138+taehuun@users.noreply.github.com>
2023-10-10 21:19:43 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 101 ms, Memory: 18,648 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -1,54 +1,72 @@
-
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st = null;
 	static StringBuilder sb = new StringBuilder();
-	static int k;
-	static int magnet[][], info[][];
+
+	static int k, magnet[][], ro[][];
 	static boolean visit[];
-	static void init() throws IOException{
+
+	static void init() throws IOException {
+
 		k = Integer.parseInt(br.readLine());
+		ro = new int[k][2];
 		magnet = new int[4][8];
-		info = new int[k][2];
 		
 		for (int i = 0; i < 4; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < 8; j++) {
-				// i = 자석번호, j = 톱날 n/s
-				// n = 0, s = 1,
 				magnet[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		for (int i = 0; i < k; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			info[i][0] = Integer.parseInt(st.nextToken())-1;	//자석번호
-			info[i][1] = Integer.parseInt(st.nextToken());	//회전방향
+			// 0번 : 자석 번호, 1 : 방향
+			ro[i][0] = Integer.parseInt(st.nextToken()) - 1;
+			ro[i][1] = Integer.parseInt(st.nextToken());
 		}
 	}
-	
+
 	static void solve() {
+
+		// n극 : 0, s극 : 1
+		// 시계방향 : 1, 반시계 -1
 		for (int i = 0; i < k; i++) {
 			visit = new boolean[4];
-			check(info[i][0], info[i][1]);
+			check(ro[i][0], ro[i][1]);
 		}
-		//정답 적는곳
-		//1번 자석 s면 1점, 2번자석 s면 2점, 3번자석 s면 4점, 4번자석 s면 8점
 		int sum = 0;
 		for (int i = 0; i < 4; i++) {
-			if(magnet[i][0] == 1) {
+			if (magnet[i][0] == 1)
 				sum += Math.pow(2, i);
-			}
 		}
 		sb.append(sum);
 	}
-	
-	//회전 메소드
+
+	static void check(int num, int dir) {
+
+		visit[num] = true;
+		
+		//왼쪽 검사
+		if(num-1>=0 && magnet[num-1][2] != magnet[num][6] && !visit[num-1]) {
+			check(num-1, dir*-1);
+		}
+		//오른쪽 검사
+		if(num+1<4 && magnet[num+1][6]!=magnet[num][2] && !visit[num+1]) {
+			check(num+1, dir*-1);
+		}
+		turn(num, dir);
+	}
+
 	static void turn(int num, int dir) {
-		//1이면 ->, -1이면 <-
+		// n극 : 0, s극 : 1
+		// 시계방향 : 1, 반시계 -1
 		if(dir == 1) {
 			int temp = magnet[num][7];
 			for (int i = 7; i > 0; i--) {
@@ -63,34 +81,10 @@ public class Solution {
 			}
 			magnet[num][7] = temp;
 		}
-	}
-	
-	//2번 idx랑 6번 idx 다른 극인지 확인하는 메소드
-	static void check(int num, int dir) {
-		visit[num] = true; // 현재 자석 방문체크
-		int reverseDir = dir*-1; // 반대 방향 설정
-
-		// 왼쪽 자석과 비교
-		if (num - 1 >= 0 && !visit[num - 1]) {
-			if (magnet[num][6] != magnet[num - 1][2]) {
-				check(num - 1, reverseDir);
-			}
-		}
-
-		// 오른쪽 자석과 비교
-		if (num + 1 < 4 && !visit[num + 1]) {
-			if (magnet[num][2] != magnet[num + 1][6]) { 
-				check(num + 1, reverseDir);
-			}
-	    }
-
-	    turn(num, dir);
+		
 	}
 
-	
-	
-	public static void main(String[] args) throws IOException{
-
+	public static void main(String[] args) throws IOException {
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			sb.append("#").append(t).append(" ");
