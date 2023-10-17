b6da95b2ae7d42ae211bab5b285c99945f7ed56c
taehuun <39759138+taehuun@users.noreply.github.com>
2023-10-17 22:05:05 +0900

    [Silver II] Title: 너구리 구구, Time: 440 ms, Memory: 116824 KB -BaekjoonHub

--- 백준/Silver/18126. 너구리 구구/너구리 구구.java
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.java
@@ -1,45 +1,48 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st = null;
+    static StringBuilder sb = new StringBuilder();
     static int[][] home;
     static int N;
+    static long max;
     static boolean[] visit;
-    static long maxDistance = 0;
     
     static void init() throws IOException{
-        N = Integer.parseInt(br.readLine());
-        
-        home = new int[N][N];
-        visit = new boolean[N];
-        for (int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
-            int c = Integer.parseInt(st.nextToken());
-            home[a][b] = c;
-            home[b][a] = c;
-        }
+    	N = Integer.parseInt(br.readLine());
+    	
+    	home = new int[N][N];
+    	visit = new boolean[N];
+    	max = 0;
+    	for (int i = 0; i < N-1; i++) {
+    		st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken())-1;
+			int b = Integer.parseInt(st.nextToken())-1;
+			int c = Integer.parseInt(st.nextToken());
+			home[a][b] = c;
+			home[b][a] = c;
+		}
     }
     
-    static void go(int v, long distance) {
-        visit[v] = true;
-        maxDistance = Math.max(maxDistance, distance);
-        
-        for (int i = 0; i < N; i++) {
-            if (home[v][i] != 0 && !visit[i]) {
-                go(i, distance + home[v][i]);
-            }
-        }
+    static void go(int v, long val) {
+    	max = Math.max(max, val);
+    	
+    	 for (int i = 0; i < N; i++) {
+             if (home[v][i] != 0 && !visit[i]) {
+            	 visit[i] = true;
+                 go(i, val + home[v][i]);
+             }
+         }
     }
 
-    public static void main(String[] args) throws IOException{
-        init();
-        visit[0] = true;
-        go(0, 0);
-        System.out.println(maxDistance);
-    }
+	public static void main(String[] args) throws IOException{
+		init();
+		visit[0] = true;
+		go(0, 0);
+		System.out.println(max);
+	}
+
 }
