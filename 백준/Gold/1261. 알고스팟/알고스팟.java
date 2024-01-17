1d76c25cc83968ee03b2022c6f8eececf288ba75
taehuun <39759138+taehuun@users.noreply.github.com>
2024-01-17 21:19:59 +0900

    [Gold IV] Title: 알고스팟, Time: 148 ms, Memory: 15208 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, -1, 1 };
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st = null;
+	static int map[][], memo[][];
+	static int N, M, shoot;
+	static boolean visit[][];
+
+	static void init() throws IOException {
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[M][N];
+		memo = new int[M][N];
+		visit = new boolean[M][N];
+		shoot = 0;
+		for (int i = 0; i < M; i++) {
+			char[] in = br.readLine().toCharArray();
+			for (int j = 0; j < N; j++) {
+				map[i][j] = in[j] - '0';
+			}
+		}
+	}
+
+	static void solve() {
+
+		Queue<int[]> que = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
+		que.offer(new int[] { 0, 0, 0 }); // x, y, shoot
+		visit[0][0] = true;
+		while (!que.isEmpty()) {
+			int[] current = que.poll();
+			int x = current[0];
+			int y = current[1];
+			int sh = current[2];
+
+			if (x == M - 1 && y == N - 1) {
+				shoot = sh;
+				return;
+			}
+
+			for (int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (check(nx, ny))
+					continue;
+				if (visit[nx][ny])
+					continue;
+
+				visit[nx][ny] = true;
+
+				if (map[nx][ny] == 1) {
+					que.offer(new int[] { nx, ny, sh + 1 });
+				} else {
+					que.offer(new int[] { nx, ny, sh });
+				}
+			}
+		}
+	}
+
+	private static boolean check(int nx, int ny) {
+		return nx < 0 || ny < 0 || nx >= M || ny >= N;
+	}
+
+	public static void main(String[] args) throws IOException {
+		init();
+		solve();
+		System.out.println(shoot);
+	}
+
+	private static void checkmap(int[][] map) {
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
