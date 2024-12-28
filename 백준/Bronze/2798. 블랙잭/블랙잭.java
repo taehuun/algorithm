4f709a7ad29fb8b64ea0e2be60370b21bbc8706a
TaeHun Kim <39759138+taehuun@users.noreply.github.com>
2024-12-28 18:06:45 +0900

    Update 블랙잭.java

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -8,50 +8,53 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st = null;
+    static StringBuilder sb = new StringBuilder();
 
     static int N, M, arrN[];
     static int anwser;
 
-    public static void init() throws IOException {
+    public static void init() throws IOException{
         st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arrN = new int[N];
+        anwser = 0;
 
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
+        for (int i=0; i<N; i++){
             arrN[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arrN);
-        anwser = 0;
     }
 
     public static void solution() {
         /*
         조합으로 3개 조합의 경우 만들어서 sum
+        key값을 넘으면 패스, key값 이하중에 최댓값 저장
          */
         combi(0, 0, 0);
         System.out.println(anwser);
     }
 
-    public static void combi(int cnt, int start, int sum) {
-        if (sum > M) return; // 현재 합이 M을 초과하면 탐색 종료
+    /*
+    조합
+     */
+    public static void combi(int cnt, int start, int sum){
+        //가지치기
+        if (sum > M) return;
 
         if (cnt == 3) {
-            anwser = Math.max(anwser, sum); // M 이하의 최대 합 업데이트
+            anwser = Math.max(anwser, sum); // M 이하의 최대 합
             return;
         }
-
+        // 조합 생성
         for (int i = start; i < N; i++) {
-            // 남은 값과 현재 sum의 합이 M보다 작거나 같을 경우만 탐색
-            if (sum + arrN[i] <= M) {
+            if (sum + arrN[i] <= M)//가지치기
                 combi(cnt + 1, i + 1, sum + arrN[i]);
-            }
         }
     }
+    public static void main(String[] args) throws IOException{
 
-    public static void main(String[] args) throws IOException {
         init();
         solution();
     }
