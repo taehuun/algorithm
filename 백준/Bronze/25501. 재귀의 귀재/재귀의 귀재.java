9f75fd535f054155632e4e85ead01cd7d1751507
TaeHun Kim <39759138+taehuun@users.noreply.github.com>
2025-01-28 16:05:26 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 204 ms, Memory: 19108 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int count;
+
+
+    public static int recursion(String s, int l, int r) {
+        count++;
+        if(l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length()-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
+            count = 0;
+            sb.append(isPalindrome(br.readLine()))
+                    .append(" ")
+                    .append(count)
+                    .append("\n");
+
+        }
+        System.out.println(sb);
+
+    }
+}
